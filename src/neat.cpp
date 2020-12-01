edfb5d009ab320d92f62089d615d42f4f40aabc0
Schlomon <salo.bee@web.de>
2020-12-01 22:28:37 +0100

    idk

--- src/neat.cpp
+++ src/neat.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
     Genome genome;
     int a, b;
-    string useless;
     cout << "Enter two numbers:" << endl;
     cin >> a >> b;
     cout << "Thanks, your result is: " << genome.test(a, b) << endl;
