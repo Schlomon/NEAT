ad75d219eaf0aaa9b143aa464844ba729a88fa84
Schlomon <salo.bee@web.de>
2020-12-01 22:13:28 +0100

    Linter test

--- src/neat.cpp
+++ src/neat.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main() {
     Genome genome;
     int a, b;
+    string useless;
     cout << "Enter two numbers:" << endl;
     cin >> a >> b;
-    cout << "Thanks, your result is: " < genome.test(a, b) << endl;
+    cout << "Thanks, your result is: " << genome.test(a, b) << endl;
     return 0;
 }
