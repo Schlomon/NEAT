d6dfcff3a725af61319eb583b690b239631c5793
Schlomon <salo.bee@web.de>
2020-12-01 22:59:56 +0100

    fix format

--- src/neat.cpp
+++ src/neat.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 int main() {
     Genome genome;
-    int a, b; cout << "Enter two numbers:" << endl;
-    cin >> a >> b;  
-        
+    int a, b;
+    cout << "Enter two numbers:" << endl;
+    cin >> a >> b;
     cout << "Thanks, your result is: " << genome.test(a, b) << endl;
     return 0;
 }
