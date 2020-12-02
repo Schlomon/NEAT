2a5fb2da1cff2a6e304a41301e67cb7de4b5e969
Schlomon <salo.bee@web.de>
2020-12-02 07:17:12 +0100

    Add copyright message

--- src/genome.cpp
+++ src/genome.cpp
@@ -1,4 +1,7 @@
+// Copyright 2020 <Robin Quendt, Salomo Hummel>
+
 #include "genome.h"
+
 int Genome::test(int a, int b) {
     return a + b;
 }
--- src/neat.cpp
+++ src/neat.cpp
@@ -1,3 +1,5 @@
+// Copyright 2020 <Robin Quendt, Salomo Hummel>
+
 #include <iostream>
 #include "genome.h"
 
